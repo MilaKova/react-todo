23c00a14d41843bbcf0391a1000ec60f53b90de3
Mila Kovach <mila@Milas-MacBook-Air.local>
2023-10-24 11:49:20 -0400

    Lifting state and props handling

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,36 +1,14 @@
 import React from 'react';
 import TodoListItem from './TodoListItem';
 
-const todoList = [
-    {
-        title: "Get up at 6am",
-        id:1,
-    },
-    {
-        title:"Drink water",
-        id:2,
-    },
-    {
-        title:"Do exercise",
-        id:3,
-    },
-    {
-        title:"Learn react",
-        id:4,
-    },
-    {
-       title:"Read a book for 30 min",
-       id:5,
-    }
-  ]
-  
-function TodoList(){
+function TodoList({todoList}){
       return(
         <ul style={{listStyleType: 'none',}}>
-          {todoList.map((item)=>(
-            <TodoListItem key={item.id} item={item}/>
+          {todoList.map((todo)=>(
+            <TodoListItem key={todo.id} item={todo}/>
           ))}
         </ul>      
       )
-    }
+    };
+    
 export default TodoList;
